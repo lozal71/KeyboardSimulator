23e8d6f88ff54e1c4fcea8d025fcb5be5b3e3189
lozal <lozovaalla@yandex.ru>
2020-04-27 22:39:59 +0300

    налажена работа клавиш CapsLock и Shift

--- KeyboardSimulator/MainWindow.xaml.cs
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -36,39 +36,35 @@ namespace KeyboardSimulator
     public partial class MainWindow : Window
     {
         public Brush tempColor;
-        public bool flagRepeat = false;
-        public bool flagTab = false;
+        //public bool flagRepeat = false;
+        //public bool flagTab = false;
         //Button tempButton;
         public MainWindow()
         {
             InitializeComponent();
+            if (Keyboard.IsKeyToggled(Key.CapsLock)) Switch_Big();
+            else Switch_Small();
         }
 
         private Button GetButton(KeyEventArgs e)
         {
             Button tempBtn = null;
             bool flagBreak = false;
+            string str;
             foreach (DockPanel dock in ButtonSet.Children)
             {
-                foreach (Button btn in dock.Children)
+                if (dock.Visibility == Visibility.Visible)
                 {
-                    if (e.Key.ToString() == btn.Name.ToString())
+                    foreach (Button btn in dock.Children)
                     {
-                        //if (btn.Background.ToString() != "#FF808080")
-                        //{
-                    //    if (btn.Name.ToString() == "Tab")
-                    //    {
-                    //        flagTab = true;
-                    //        flagBreak = true;
-                    //        break;
-                    //    }
-                    ////}
-                    //    else
-                    //    {
+                        str = btn.Name.ToString().Split('_')[0];
+                        if (e.Key.ToString() == btn.Name.ToString() ||
+                            e.Key.ToString() + "_shift" == btn.Name.ToString())
+                        {
                             tempBtn = btn;
                             flagBreak = true;
                             break;
-                        //}
+                        }
                     }
                 }
                 if (flagBreak)
@@ -78,66 +74,192 @@ namespace KeyboardSimulator
             }
             return tempBtn;
         }
-        private void WritePanel_KeyDown(object sender, KeyEventArgs e)
+        private void Switch_Big()
         {
-            Button tempButton = GetButton(e);
-            if (!e.IsRepeat)
-            {
-                //WritePanel.Text += tempButton.Background.ToString(); // e.Key.ToString();
-                if (tempButton != null)
-                {
-                    //string name = ResourceHelper.FindNameFromResource(this.Resources, tempButton);
-                    tempColor = tempButton.Background;
+            Dock1.Visibility = Visibility.Hidden;
+            Dock11.Visibility = Visibility.Visible;
+            Dock2.Visibility = Visibility.Hidden;
+            Dock22.Visibility = Visibility.Visible;
+            Dock3.Visibility = Visibility.Hidden;
+            Dock33.Visibility = Visibility.Visible;
+        }
+        private void Switch_Big_Shift()
+        {
+            Dock0.Visibility = Visibility.Hidden;
+            Dock00.Visibility = Visibility.Visible;
+            Dock1.Visibility = Visibility.Hidden;
+            Dock11.Visibility = Visibility.Visible;
+            Dock2.Visibility = Visibility.Hidden;
+            Dock22.Visibility = Visibility.Visible;
+            Dock3.Visibility = Visibility.Hidden;
+            Dock33.Visibility = Visibility.Visible;
+        }
+        private void Switch_Big_Shift_Digit_Small()
+        {
+            Dock00.Visibility = Visibility.Hidden;
+            Dock0.Visibility = Visibility.Visible;
+            Dock1.Visibility = Visibility.Hidden;
+            Dock11.Visibility = Visibility.Visible;
+            Dock2.Visibility = Visibility.Hidden;
+            Dock22.Visibility = Visibility.Visible;
+            Dock3.Visibility = Visibility.Hidden;
+            Dock33.Visibility = Visibility.Visible;
+        }
+        private void Switch_Small()
+        {
+            Dock11.Visibility = Visibility.Hidden;
+            Dock1.Visibility = Visibility.Visible;
+            Dock22.Visibility = Visibility.Hidden;
+            Dock2.Visibility = Visibility.Visible;
+            Dock33.Visibility = Visibility.Hidden;
+            Dock3.Visibility = Visibility.Visible;
+        }
+        private void Switch_Small_Shift()
+        {
+            Dock00.Visibility = Visibility.Hidden;
+            Dock0.Visibility = Visibility.Visible;
+            Dock11.Visibility = Visibility.Hidden;
+            Dock1.Visibility = Visibility.Visible;
+            Dock22.Visibility = Visibility.Hidden;
+            Dock2.Visibility = Visibility.Visible;
+            Dock33.Visibility = Visibility.Hidden;
+            Dock3.Visibility = Visibility.Visible;
+        }
+        private void Switch_Small_Shift_Digit_Big()
+        {
+            Dock0.Visibility = Visibility.Hidden;
+            Dock00.Visibility = Visibility.Visible;
+            Dock11.Visibility = Visibility.Hidden;
+            Dock1.Visibility = Visibility.Visible;
+            Dock22.Visibility = Visibility.Hidden;
+            Dock2.Visibility = Visibility.Visible;
+            Dock33.Visibility = Visibility.Hidden;
+            Dock3.Visibility = Visibility.Visible;
+        }
 
+        private void Switch_Case()
+        {
+            // если включен CapsLock
+            if (Keyboard.IsKeyToggled(Key.CapsLock))
+            {
+                // показывем кнопки верхнего регистра для всех, кроме кнопок цифрового ряда 
+                Switch_Big();
+                // если нажат Shift
+                if (Keyboard.IsKeyDown(Key.LeftShift) ||
+                    Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    // переключаем регистр кнопок цифрового ряда
+                    //if (Dock0.Visibility == Visibility.Visible)
+                    //{
+                        Dock0.Visibility = Visibility.Hidden;
+                        Dock00.Visibility = Visibility.Visible;
+                    //}
+                    //else
+                    //{
+                    //    Dock00.Visibility = Visibility.Hidden;
+                    //    Dock0.Visibility = Visibility.Visible;
+                    //}
+                    // переключаем на верхний регистр все остальные кнопки
+                    Switch_Small();
                 }
             }
+            else
+            {
+                Switch_Small();
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    // переключаем регистр кнопки цифрового ряда
+                    //if (Dock0.Visibility == Visibility.Visible)
+                    //{
+                    //    Dock0.Visibility = Visibility.Hidden;
+                    //    Dock00.Visibility = Visibility.Visible;
+                    //}
+                    //else
+                    //{
+                        Dock00.Visibility = Visibility.Hidden;
+                        Dock0.Visibility = Visibility.Visible;
+                    //}
+                    Switch_Big();
+                }
+            }
+
+        }
+        private void WritePanel_KeyDown(object sender, KeyEventArgs e)
+        {
+            // определяем кнопку по клавише
+            Button tempButton = GetButton(e);
+            // если кнопка определилась
             if (tempButton != null)
             {
-                tempButton.Background = Brushes.White;
+                // меняем цвет фона на белый
+                if (e.Key.ToString() != "Capital")
+                {
+                    tempButton.Background = Brushes.White;
+                }
+                // если включен CapsLock
+                if (Keyboard.IsKeyToggled(Key.CapsLock))
+                {
+                    // если нажат Shift
+                    if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                    {
+                        Switch_Small_Shift_Digit_Big(); //регистр всех кнопок-нижний, цифровой ряд - верхний
+                    }
+                    else
+                    {
+                        Switch_Big(); // регистр всех кнопок, кроме цифрового ряда - верхний
+                    }
+                }
+                else // если CapsLock выключен
+                {
+                    // если нажат Shift
+                    if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                    {
+                        Switch_Big_Shift(); // регистр всех кнопок - верхний
+                    }
+                    else
+                    {
+                        Switch_Small_Shift(); // регистр всех кнопок - нижний
+                    }
+                }
             }
-            if (e.IsRepeat)
-            {
-                flagRepeat = true;
-            }
-            //if (flagTab)
-            //{
-            //    WritePanel.Focus();
-            //    WritePanel.Select(WritePanel.Text.Length, 1);
-            //}
-
         }
         private void WritePanel_KeyUp(object sender, KeyEventArgs e)
         {
             Button tempButton = GetButton(e);
-            if (tempButton != null && tempButton.Name.ToString() != "Back")
+            if (tempButton != null) 
             {
-                //tempButton.Background = tempColor;
                 tempButton.Background = tempButton.BorderBrush;
-                flagRepeat = false;
+                // если включен CapsLock
+                if (Keyboard.IsKeyToggled(Key.CapsLock))
+                {
+                    // если отжат Shift
+                    if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
+                    {
+                        Switch_Big_Shift_Digit_Small(); // регистр всех кнопок - верхний, цифровой - нижний
+                    }
+                    else
+                    {
+                        Switch_Small_Shift(); // регистр всех кнопок - нижний
+                    }
+                }
+                else // если CapsLock выключен
+                {
+                    // если отжат Shift
+                    if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
+                    {
+                        Switch_Small_Shift(); // регистр всех кнопок - нижний
+                    }
+                    else
+                    {
+                        Switch_Big_Shift(); // регистр всех кнопок - верхний
+                    }
+                }
             }
-            //tempButton = null;
+            tempButton = null;
         }
         private void WritePanel_TextInpt(object sender, TextCompositionEventArgs e)
         {
-            //string tempSrtring;
-            //string str;
-            //int len;
-            if (!flagRepeat)
-            {
-                //if (!flagBack)
-                //{
-                    WritePanel.Text += e.Text;
-                    flagRepeat = true;
-                //}
-                //else
-                //{
-                //    str = WritePanel.Text.ToString();
-                //    len = str.Length;
-                //    tempSrtring = str.Remove(len - 1);
-                //    WritePanel.Text = tempSrtring;
-                //    flagBack = false;
-                //}
-            }
+            WritePanel.Text += e.Text;   
         }
     }
 }
