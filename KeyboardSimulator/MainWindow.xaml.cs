9c8b69b072d2a8e5d153f76f3d85148c9985970b
lozal <lozovaalla@yandex.ru>
2020-05-04 22:32:25 +0300

    начало работы с кнопками Старт и Стоп

--- KeyboardSimulator/MainWindow.xaml.cs
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -25,11 +25,19 @@ namespace KeyboardSimulator
             public Button self;
             public Button shift;
         }
+
+        public bool flagStart;
         public MainWindow()
         {
             InitializeComponent();
             if (Keyboard.IsKeyToggled(Key.CapsLock)) Switch_Big();
             else Switch_Small();
+            flagStart = false;
+            WritePanel.IsEnabled = false;
+            btnStart.IsEnabled = true;
+            btnStart.Opacity = 1.0;
+            btnStop.IsEnabled = false;
+            btnStop.Opacity = 0.5;
         }
         private ButtonDouble GetButtons(KeyEventArgs e)
         {
@@ -68,7 +76,6 @@ namespace KeyboardSimulator
             }
             return tempBtn;
         }
-
         private Button GetButton(KeyEventArgs e)
         {
             Button tempBtn = null;
@@ -208,88 +215,119 @@ namespace KeyboardSimulator
         }
         private void WritePanel_KeyDown(object sender, KeyEventArgs e)
         {
-            // определяем кнопку по клавише
-            Button tempButton = GetButton(e);
-            // если кнопка определилась
-            if (tempButton != null)
+            if (flagStart)
             {
-                // если это не кнопка CapsLock
-                if (e.Key.ToString() != "Capital" &&
-                    !e.Key.ToString().Contains("Ctrl") && 
-                    !e.Key.ToString().Contains("Win") &&
-                    !e.Key.ToString().Contains("Alt"))
+                // определяем кнопку по клавише
+                Button tempButton = GetButton(e);
+                // если кнопка определилась
+                if (tempButton != null)
                 {
-                    // меняем цвет фона на белый
-                    tempButton.Background = Brushes.White;
-                    //tempButton.Opacity = 0.8;
-                }
-                // если включен CapsLock
-                if (Keyboard.IsKeyToggled(Key.CapsLock))
-                {
-                    // если нажат Shift
-                    if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                    // если это не кнопка CapsLock
+                    if (e.Key.ToString() != "Capital" &&
+                        !e.Key.ToString().Contains("Ctrl") &&
+                        !e.Key.ToString().Contains("Win") &&
+                        !e.Key.ToString().Contains("Alt"))
                     {
-                        Switch_Small_Shift_Digit_Big(); //регистр всех кнопок-нижний, цифровой ряд - верхний
+                        // меняем цвет фона на белый
+                        tempButton.Background = Brushes.White;
+                        //tempButton.Opacity = 0.8;
                     }
-                    else
+                    // если включен CapsLock
+                    if (Keyboard.IsKeyToggled(Key.CapsLock))
                     {
-                        Switch_Big(); // регистр всех кнопок, кроме цифрового ряда - верхний
+                        // если нажат Shift
+                        if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                        {
+                            Switch_Small_Shift_Digit_Big(); //регистр всех кнопок-нижний, цифровой ряд - верхний
+                        }
+                        else
+                        {
+                            Switch_Big(); // регистр всех кнопок, кроме цифрового ряда - верхний
+                        }
                     }
-                }
-                else // если CapsLock выключен
-                {
-                    // если нажат Shift
-                    if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                    else // если CapsLock выключен
                     {
-                        Switch_Big_Shift(); // регистр всех кнопок - верхний
-                    }
-                    else
-                    {
-                        Switch_Small_Shift(); // регистр всех кнопок - нижний
+                        // если нажат Shift
+                        if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                        {
+                            Switch_Big_Shift(); // регистр всех кнопок - верхний
+                        }
+                        else
+                        {
+                            Switch_Small_Shift(); // регистр всех кнопок - нижний
+                        }
                     }
                 }
             }
         }
         private void WritePanel_KeyUp(object sender, KeyEventArgs e)
         {
-            ButtonDouble tempButton = GetButtons(e);
-            if (tempButton.self != null && tempButton.shift != null)
+            if (flagStart)
             {
-                tempButton.self.Background = tempButton.self.BorderBrush;
-                tempButton.shift.Background = tempButton.shift.BorderBrush;
-                // если включен CapsLock
-                if (Keyboard.IsKeyToggled(Key.CapsLock))
+                ButtonDouble tempButton = GetButtons(e);
+                if (tempButton.self != null && tempButton.shift != null)
                 {
-                    // если отжат Shift
-                    if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
+                    tempButton.self.Background = tempButton.self.BorderBrush;
+                    tempButton.shift.Background = tempButton.shift.BorderBrush;
+                    // если включен CapsLock
+                    if (Keyboard.IsKeyToggled(Key.CapsLock))
                     {
-                        Switch_Big_Shift_Digit_Small(); // регистр всех кнопок - верхний, цифровой - нижний
+                        // если отжат Shift
+                        if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
+                        {
+                            Switch_Big_Shift_Digit_Small(); // регистр всех кнопок - верхний, цифровой - нижний
+                        }
+                        else
+                        {
+                            Switch_Small_Shift(); // регистр всех кнопок - нижний
+                        }
                     }
-                    else
+                    else // если CapsLock выключен
                     {
-                        Switch_Small_Shift(); // регистр всех кнопок - нижний
+                        // если отжат Shift
+                        if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
+                        {
+                            Switch_Small_Shift(); // регистр всех кнопок - нижний
+                        }
+                        else
+                        {
+                            Switch_Big_Shift(); // регистр всех кнопок - верхний
+                        }
                     }
                 }
-                else // если CapsLock выключен
-                {
-                    // если отжат Shift
-                    if (Keyboard.IsKeyUp(Key.LeftShift) || Keyboard.IsKeyUp(Key.RightShift))
-                    {
-                        Switch_Small_Shift(); // регистр всех кнопок - нижний
-                    }
-                    else
-                    {
-                        Switch_Big_Shift(); // регистр всех кнопок - верхний
-                    }
-                }
+                tempButton.self = null;
+                tempButton.shift = null;
             }
-            tempButton.self = null;
-            tempButton.shift = null;
         }
 
         private void WritePanel_TextInpt(object sender, TextCompositionEventArgs e)
         {
-            WritePanel.Text += e.Text;
+            if (flagStart)
+            {
+                WritePanel.Text += e.Text;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            flagStart = true;
+            ReadPanel.Text = "I love you";
+            btnStart.IsEnabled = false;
+            btnStart.Opacity = 0.5;
+            btnStop.IsEnabled = true;
+            btnStop.Opacity = 1.0;
+            WritePanel.IsEnabled = true;
+            WritePanel.Focus();
+        }
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            flagStart = false;
+            btnStart.IsEnabled = true;
+            btnStart.Opacity = 1.0;
+            btnStop.IsEnabled = false;
+            btnStop.Opacity = 0.5;
+            WritePanel.IsEnabled = false;
+            WritePanel.Focus();
         }
     }
 }
